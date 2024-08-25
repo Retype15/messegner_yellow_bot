e80ac21e88c81af3ab1706532e59df886114d3c2
rety <reynierramos280@gmail.com>
2024-08-25 10:55:30 -0700

    v1.0

---
+++ main.py
@@ -0,0 +1,286 @@
+import csv
+from datetime import datetime
+from deep_translator import GoogleTranslator
+import pytz
+import re
+from telegram import Update, InputFile, InlineKeyboardButton, InlineKeyboardMarkup, Bot, BotCommand
+from telegram.ext import ApplicationBuilder,CallbackContext, CommandHandler, MessageHandler, filters, ContextTypes, CallbackQueryHandler, CallbackContext
+from openpyxl import Workbook, load_workbook
+import os
+from texts import TEXTS
+
+#############################--GLOBAL-VARS--#############################################
+
+GRUPOS = {}
+
+HELP = """
+Ayuda:
+    
+
+"""
+##############################--SECURITY--################################################
+
+async def is_admin() -> bool:
+    user_id = update.effective_user.id
+    chat_id = update.effective_chat.id
+
+    # Verificar si el usuario es un administrador en el grupo
+    chat_member = await context.bot.get_chat_member(chat_id, user_id)
+    if chat_member.status not in ['administrator', 'creator']:
+        await update.message.reply_text(get_text(update,'no_permission'))
+        return
+
+#############################--FUNCIONES--################################################
+
+def get_text(update: Update, key):
+    """Obtiene el texto en el idioma preferido del usuario o detecta el idioma automáticamente."""
+    lang = ""
+    ids = 0.0
+    chat_id = update.effective_chat.id
+    for name, datos in GRUPOS.items():
+        if datos['chat_id'] == chat_id:
+            lang = GRUPOS[name]['lang']
+            return TEXTS[lang].get(key, TEXTS[lang]['default'])
+    
+    lang = detect_language(update.effective_user.language_code)
+    GRUPOS[name]['lang'] = lang  # Guardar la detección automática en el diccionario
+        
+    return TEXTS[lang].get(key, TEXTS[lang]['default'])
+
+def guardar_datos_csv():
+    with open('grupos_data.csv', mode='w', newline='', encoding='utf-8') as file:
+        writer = csv.writer(file)
+        # Escribir encabezados
+        writer.writerow(['Grupo', 'Chat ID', 'lang'])
+        
+        # Escribir datos de cada grupo
+        for nombre, datos in GRUPOS.items():
+            writer.writerow([nombre, datos['chat_id'], datos['lang']])
+
+def cargar_datos_csv() -> None:
+    try:
+        with open('grupos_data.csv', mode='r', newline='', encoding='utf-8') as file:
+            reader = csv.reader(file)
+            if reader is None: return
+            next(reader)  # Saltar el encabezado
+            for row in reader:
+                nombre, chat_id, lang = row
+                GRUPOS[nombre] = {
+                    'chat_id': int(chat_id),
+                    'count': 0,
+                    'lang': lang,
+                    'users': []
+                }
+    except Exception as e:
+        print(f"Ocurrió un error al leer el archivo CSV: {e}")
+
+################################--LANGUAGE--##############################################
+
+async def set_language(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None: # Comando para establecer el idioma
+    if not is_admin: return []
+    chat_id = update.effective_chat.id
+    name = ""
+    for nombre, datos in GRUPOS.items():
+        if datos['chat_id'] == chat_id:
+            name = nombre
+            break
+    
+    if context.args:
+        txt = ' '.join(context.args).strip()
+        if txt == 'en':
+            GRUPOS[name]['lang'] = 'en'
+            await update.message.reply_text(get_text(update, 'language_selected'))
+            guardar_datos_csv()
+        elif txt == 'es':
+            GRUPOS[name]['lang'] = 'es'
+            await update.message.reply_text(get_text(update, 'language_selected'))
+            guardar_datos_csv()
+        elif txt == 'ru':
+            GRUPOS[name]['lang'] = 'ru'
+            await update.message.reply_text(get_text(update, 'language_selected'))
+            guardar_datos_csv() 
+        else:
+            await update.message.reply_text(get_text(update, 'language_non_exist'))
+            #lenguaje no reconocido
+    else:
+        await update.message.reply_text(get_text(update, 'set_language_bad'))
+        
+def detect_language(language_code):
+    """Detecta el idioma del usuario basado en el código de idioma proporcionado por Telegram."""
+    if language_code is None:
+        return 'en'
+    elif language_code.startswith('es'):
+        return 'es'
+    elif language_code.startswith('ru'):
+        return 'ru'
+    else:
+        return 'en'
+
+################################--BOT--###################################################
+
+# Función para reiniciar la información de todos los grupos
+async def reset(update: Update, context: CallbackContext) -> None:
+    if not is_admin: return []
+    for nombre, datos in GRUPOS.items():
+        datos['count'] = 0
+        datos['users'] = []
+    await update.message.reply_text(get_text(update, 'reset_info'))
+
+async def order(update: Update, context: CallbackContext) -> None:
+    if not is_admin: return []
+    bot: Bot = context.bot
+
+    # Verificar que el mensaje esté citado
+    if not update.message.reply_to_message:
+        await update.message.reply_text(get_text(update, 'order_no_cited'))
+        return
+
+    # Obtener el mensaje citado
+    mensaje = update.message.reply_to_message.text
+
+    # Obtener el texto del comando
+    texto_comando = update.message.text.strip()
+    
+    # Separar el comando del mensaje
+    partes = texto_comando.split(' ', 1)
+    
+    if len(partes) < 2:
+        await update.message.reply_text(get_text(update, 'order_incorrect'))
+        return
+
+    # Obtener los nombres de los grupos
+    grupos = partes[1].split(',')
+
+    # Enviar el mensaje a los grupos especificados
+    for grupo in grupos:
+        grupo = grupo.strip()  # Eliminar espacios adicionales
+        if grupo == 'ALL':
+            # Enviar mensaje a todos los grupos
+            for nombre, datos in GRUPOS.items():
+                chat_id = datos['chat_id']
+                text = GoogleTranslator(source='auto', target= str(GRUPOS[nombre]['lang'])).translate(mensaje)
+                text += get_text(update, 'order_no_participants')
+                keyboard = [[InlineKeyboardButton(get_text(update, 'i_will_participate'), callback_data=str(chat_id))]]
+                reply_markup = InlineKeyboardMarkup(keyboard)
+                GRUPOS[nombre]['count'] = 0
+                GRUPOS[nombre]['users'] = []
+                await bot.send_message(chat_id=chat_id, text=text, reply_markup=reply_markup)
+        elif grupo in GRUPOS:
+            # Enviar mensaje al grupo especificado
+            chat_id = GRUPOS[grupo]['chat_id']
+            text = GoogleTranslator(source='auto', target= str(GRUPOS[grupo]['lang'])).translate(mensaje)
+            text += get_text(update, 'order_no_participants')
+            keyboard = [[InlineKeyboardButton(get_text(update, 'i_will_participate'), callback_data=str(chat_id))]]
+            reply_markup = InlineKeyboardMarkup(keyboard)
+            GRUPOS[grupo]['count'] = 0
+            GRUPOS[nombre]['users'] = []
+            await bot.send_message(chat_id=chat_id, text=text, reply_markup=reply_markup)
+        else:
+            await update.message.reply_text(get_text(update,'order_no_group_founded').format(grupo=grupo))
+
+# Función para agregar el chat al bot_data y al diccionario GRUPOS
+async def start(update: Update, context: CallbackContext) -> None:
+    await update.message.reply_text(get_text(update, 'welcome'))
+
+async def help(update: Update, context: CallbackContext) -> None:
+    await update.message.reply_text(get_text(update, 'help_message'))
+
+async def register(update: Update, context: CallbackContext) -> None:
+    if not is_admin: return []
+    chat_id = update.effective_chat.id
+    chat_title = update.effective_chat.title or "No name"
+    nombre_grupo = ''
+    lang = ''
+    
+    # Obtener el nombre del grupo desde el comando
+    if context.args:
+        context = ' '.join(context.args).strip()
+        parts = context.split(' ', 2)
+        nombre_grupo = parts[0] if len(parts) > 0 else chat_title
+        lang = parts[1] if len(parts) > 1 else 'en'
+    else:
+        nombre_grupo = chat_title
+        lang = 'en'
+
+    # Guardar el grupo en GRUPOS
+    GRUPOS[nombre_grupo] = {
+        'chat_id': chat_id,
+        'count': 0,
+        'lang': lang,
+        'users': []
+    }
+    guardar_datos_csv()
+    
+    await update.message.reply_text(get_text(update, 'group_saved').format(nombre_grupo=nombre_grupo))
+    
+async def squads(update: Update, context: CallbackContext) -> None:
+    if not is_admin: return []
+    if not GRUPOS:
+        await update.message.reply_text(get_text(update, 'no_group_saved'))
+        return
+
+    mensaje = get_text(update, 'actual_groups')
+    for nombre, datos in GRUPOS.items():
+        mensaje += get_text(update, 'squads_info_group').format(name=nombre,count=datos['count'],lang=datos['lang'])
+    
+    await update.message.reply_text(mensaje)
+
+# Función que maneja el callback del botón
+async def boton_callback(update: Update, context: CallbackContext) -> None:
+    query = update.callback_query
+    user_id = query.from_user.id
+    chat_id = int(query.data)
+
+    await query.answer()
+
+    # Encontrar el grupo correspondiente al chat_id
+    for nombre, datos in GRUPOS.items():
+        if datos['chat_id'] == chat_id:
+            # Verificar si el usuario ya presionó el botón
+            if user_id not in datos['users']:
+                GRUPOS[nombre]['count'] += 1
+                datos['users'].append(user_id)  # Añadir el user_id al arreglo
+                text = query.message.text
+                lineas = text.split("\n")
+                lineas[-1] = get_text(update, 'button_squad_participants').format(name=nombre,count=GRUPOS[nombre]['count'])
+                text = '\n'.join(lineas)
+                await query.edit_message_text( text=text,
+                    reply_markup=InlineKeyboardMarkup(
+                        [[InlineKeyboardButton(get_text(update, 'i_will_participate'), callback_data=str(chat_id))]]
+                    )
+                )
+            else:
+                await query.answer(text=get_text(update, 'button_has_pressed'), show_alert=True)
+            break
+    else:
+        await query.edit_message_text( text=text,
+                    reply_markup=InlineKeyboardMarkup(
+                        [[InlineKeyboardButton(get_text(update, 'group_not_found'), callback_data=str(chat_id))]]
+                    ))
+
+async def remove(update: Update, context: CallbackContext) -> None:
+    if not is_admin: return []
+    nombre_grupo = ' '.join(context.args).strip()
+    if nombre_grupo in GRUPOS:
+        del GRUPOS[nombre_grupo]
+        await update.message.reply_text(get_text(update, 'group_has_eliminated').format(nombre_grupo=nombre_grupo))
+        guardar_datos_csv()
+    else:
+        await update.message.reply_text(get_text(update, 'group_not_found'))
+
+#############################--MAIN--####################################################
+
+cargar_datos_csv()
+app = ApplicationBuilder().token("7523544789:AAE6u1waeC3kL3LpZK_7-J_CNqNTdPbybG4").build()
+
+app.add_handler(CommandHandler("start", start))
+app.add_handler(CommandHandler("help", help))
+app.add_handler(CommandHandler("order", order))
+app.add_handler(CommandHandler("register", register))
+app.add_handler(CommandHandler("squads", squads))
+app.add_handler(CommandHandler("reset", reset))
+app.add_handler(CommandHandler("remove", remove))
+app.add_handler(CommandHandler("set_language", set_language))
+app.add_handler(CallbackQueryHandler(boton_callback))
+
+app.run_polling()
---
+++ texts.py
@@ -0,0 +1,100 @@
+
+TEXTS = {
+    'es': {
+        'welcome': "HOLA, soy el BOT mensajero! me encargo de que todas las órdenes de batalla lleguen a todos sin retraso!",
+        'no_permission': "No tienes permisos para usar este comando.",
+        'language_selected': "Has seleccionado el idioma español, ¡genial!",
+        'language_non_exist': "Lenguaje no reconocido, por el momento solo está disponible (en, es, ru).",
+        'set_language_bad': "¡Por favor, proporcione su lenguaje correctamente! Ej: (/set_language en)",
+        'reset_info': "¡Toda la información de los grupos ha sido reiniciada!",
+        'order_no_cited': "¡Por favor, cita el mensaje que quieres enviar!",
+        'order_incorrect': "Uso incorrecto. Usa: /order <grupos>... Respondiendo a un mensaje",
+        'order_no_participants': "\nxx-xx-xx-xx-xx-xx-xx-xx-xx-xx-xx\nAún no hay participantes, ¿a qué esperas? ¡Únete al plan de batalla!",
+        'i_will_participate': "¡Yo participaré!",
+        'order_no_group_founded': "Grupo '{grupo}' no encontrado.",
+        'group_saved': "Grupo guardado como '{nombre_grupo}'.",
+        'no_group_saved': "No hay grupos guardados.",
+        'actual_groups': "Grupos actuales:\n",
+        'squads_info_group': "{name}: participantes: {count}, lenguaje: {lang}\n",
+        'button_squad_participants': "¡Participantes en el escuadrón de {name} ahora son {count}!",
+        'button_has_pressed': "¡Ya has presionado este botón antes! 🎉",
+        'group_not_found': "No se encontró el grupo correspondiente.",
+        'group_has_eliminated': "Grupo '{nombre_grupo}' eliminado.",
+        'default': "ERROR:XXX>Lo siento, el texto solicitado no está disponible.",
+        'help_message': (
+            'Ayuda:\n'
+            'Usa /order + <escuadrones> para publicar una orden en su respectivo grupo\n'
+            'Usa /register + <nombre sencillo> + <idioma: en, es, ru> para registrar a tu squad al bot mensajero\n'
+            'Usa /squads para obtener la lista de los escuadrones actualmente disponibles y cuantos están listos para luchar!\n'
+            'Usa /reset para restablecer la información de votos en caso de haber algún problema\n"'
+            'Usa /remove para eliminar un gremio o escuadrón de la lista!\n'
+            'Usa /set_language para cambiar el idioma del bot.'
+        ),
+    },
+    'en': {
+        'welcome': "HELLO, I am the Messenger BOT! I make sure that all battle orders reach everyone without delay!",
+        'no_permission': "You don't have permission to use this command.",
+        'language_selected': "You have selected English, great!",
+        'language_non_exist': "Unrecognized language, currently only available (en, es, ru).",
+        'set_language_bad': "Please provide your language correctly! Example: (/set_language en)",
+        'reset_info': "All group information has been reset!",
+        'order_no_cited': "Please quote the message you want to send!",
+        'order_incorrect': "Incorrect use. Use: /order <groups>... While replying to a message",
+        'order_no_participants': "\nxx-xx-xx-xx-xx-xx-xx-xx-xx-xx-xx\nNo participants yet, what are you waiting for? Join the battle plan!",
+        'i_will_participate': "I will participate!",
+        'order_no_group_founded': "Group '{grupo}' not found.",
+        'group_saved': "Group saved as '{nombre_grupo}'.",
+        'no_group_saved': "No groups saved.",
+        'actual_groups': "Current groups:\n",
+        'squads_info_group': "{name}: participants: {count}, language: {lang}\n",
+        'button_squad_participants': "Participants in the {name} squad are now {count}!",
+        'button_has_pressed': "You have already pressed this button before! 🎉",
+        'group_not_found': "No corresponding group was found.",
+        'group_has_eliminated': "Group '{nombre_grupo}' has been deleted.",
+        'default': "ERROR:XXX>Sorry, the requested text is not available.",
+        'help_message': (
+            'Help:\n'
+            'Use /order + <squads> to publish an order in their respective group\n'
+            'Use /register + <simple name> + <language: en, es, ru> to register your squad to the messenger bot\n'
+            'Use /squads to get the list of currently available squads and how many are ready to fight!\n'
+            'Use /reset to reset voting information in case of any problem\n'
+            'Use /remove to remove a guild or squad from the list!\n'
+            'Use /set_language to change the bot\'s language.'
+        ),
+    },
+    'ru': {
+        'welcome': "ПРИВЕТ, я BOT-посланник! Я забочусь о том, чтобы все боевые приказы доходили до всех без задержек!",
+        'no_permission': "У вас нет прав на использование этой команды.",
+        'language_selected': "Вы выбрали русский язык, отлично!",
+        'language_non_exist': "Язык не распознан, в настоящее время доступен только (en, es, ru).",
+        'set_language_bad': "Пожалуйста, укажите ваш язык правильно! Пример: (/set_language en)",
+        'reset_info': "Вся информация о группах была сброшена!",
+        'order_no_cited': "Пожалуйста, цитируйте сообщение, которое хотите отправить!",
+        'order_incorrect': "Неправильное использование. Используйте: /order <группы>... Отвечая на сообщение",
+        'order_no_participants': "\nxx-xx-xx-xx-xx-xx-xx-xx-xx-xx-xx\nПока нет участников, чего вы ждете? Присоединяйтесь к боевому плану!",
+        'i_will_participate': "Я буду участвовать!",
+        'order_no_group_founded': "Группа '{grupo}' не найдена.",
+        'group_saved': "Группа сохранена как '{nombre_grupo}'.",
+        'no_group_saved': "Группы не сохранены.",
+        'actual_groups': "Текущие группы:\n",
+        'squads_info_group': "{name}: участники: {count}, язык: {lang}\n",
+        'button_squad_participants': "Участники в отряде {name} теперь составляют {count}!",
+        'button_has_pressed': "Вы уже нажали эту кнопку ранее! 🎉",
+        'group_not_found': "Соответствующая группа не найдена.",
+        'group_has_eliminated': "Группа '{nombre_grupo}' удалена.",
+        'default': "ОШИБКА:XXX>Извините, запрашиваемый текст недоступен.",
+        'help_message': (
+            'Помощь:\n'
+            'Используйте /order + <отряды>, чтобы опубликовать приказ в соответствующей группе\n'
+            'Используйте /register + <простое имя> + <язык: en, es, ru>, чтобы зарегистрировать свой отряд в посланнике\n'
+            'Используйте /squads, чтобы получить список доступных отрядов и узнать, сколько готовы к бою!\n'
+            'Используйте /reset, чтобы сбросить информацию о голосовании в случае проблемы\n'
+            'Используйте /remove, чтобы удалить гильдию или отряд из списка!\n'
+            'Используйте /set_language, чтобы изменить язык бота.'
+        ),
+    }
+}
+
+# Diccionario de idiomas soportados
+SUPPORTED_LANGUAGES = ['en', 'es', 'ru']
+
